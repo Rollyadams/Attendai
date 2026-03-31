027e30ab6da16c32692bb756e45dc8db419b280b
Rolayo Adamson <rollyadamstechworld@gmail.com>
2026-03-31 02:24:38 +0100

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://zwxgyyebrxfljvxosnuu.supabase.co'
-const supabaseAnonKey = 'sb_publishable_PJ7DK6Rq8hyqgxIFgCED9w_w1LYzeEf'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp3eGd5eWVicnhmbGp2eG9zbnV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4ODY5OTAsImV4cCI6MjA5MDQ2Mjk5MH0.jLlBqe2PKTMQZ6U66Z5JcK36HDKYuEFTqco3qUXk4Ns'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
-
